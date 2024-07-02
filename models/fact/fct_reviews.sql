f9f8b51ce770eb8b12722b965df2fa6ec23d3686
rsbarhey <barheyan@yahoo.com>
2024-07-02 14:41:26 -0600

    using third party packages

--- models/fact/fct_reviews.sql
+++ models/fact/fct_reviews.sql
@@ -8,7 +8,11 @@
 WITH src_reviews AS (
     SELECT * FROM {{ref('src_reviews')}}
 )
-SELECT * FROM src_reviews
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text'])}} as review_id,
+    *
+FROM 
+    src_reviews
 WHERE review_text IS NOT NULL
 
 {% if is_incremental() %}
